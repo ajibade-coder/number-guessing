e00ca3e81f4fc2b0cc4fe29041459c75e2c5e03b
ajibade-coder <meriesbells1996@gmail.com>
2024-08-29 11:29:36 -0400

    test: user_input

--- number_guess.sh
+++ number_guess.sh
@@ -11,5 +11,33 @@ read USERNAME_INPUT
 
 echo $USERNAME_INPUT
 
+# querying data base to find username from input gotten
+PLAYER_NAME=$($PSQL "SELECT * FROM users JOIN games USING(player_id)")
+
+
+# func for the guessing_game
+GUESSING_GAME() {
+  
+
+}
+
+
 # generating  a random number from 1 to 1000
 RANDOM_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+
+# if player name not found, then let register play in database
+if [[ -z $PLAYER_NAME ]]
+then
+  echo "Welcome, $USERNAME_INPUT! It looks like this is your first time here."
+
+  # if player is found
+else
+
+    echo "Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses."
+
+  echo ""
+
+fi
+
+
